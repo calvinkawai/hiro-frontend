14bbe728efa2b07f1c53cff3cc98f2c38eba4002
Albert Cui <albert@benchsci.com>
2021-04-20 23:54:14 -0400

    Add support for Back4App (Parse)

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,17 @@
 import * as React from "react"
+import { Link } from "gatsby"
+import Parse from "parse"
+
+Parse.initialize(process.env.GATSBY_APPLICATION_ID, process.env.GATSBY_JAVASCRIPT_KEY);
+Parse.serverURL = "https://parseapi.back4app.com/";
+
 
 // markup
 const IndexPage = () => {
   return (
     <nav class="navbar navbar-dark bg-dark">
       <a class="navbar-brand" href="#">HIRO</a>
-      <button type="button" class="btn btn-dark">Login</button>
+      <Link to="/register/" type="button" class="btn btn-dark">Register</Link>
     </nav>
   )
 }
