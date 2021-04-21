e5ba1bbfb96774bd7a63e946564cf3d1b6d11ab4
calvinkawai <ljw361@hotmail.com>
2021-04-21 01:32:07 -0400

    test env var

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,13 @@ import * as React from "react"
 // markup
 const IndexPage = () => {
   return (
-    <nav class="navbar navbar-dark bg-dark">
-      <a class="navbar-brand" href="#">HIRO</a>
-      <button type="button" class="btn btn-dark">Login</button>
-    </nav>
+    <div>
+      <nav class="navbar navbar-dark bg-dark">
+        <a class="navbar-brand" href="#home">HIRO</a>
+        <button type="button" class="btn btn-dark">Login</button>
+      </nav>
+      <samp>{process.env.GATSBY_APPLICATION_ID}</samp>
+    </div>
   )
 }
 
