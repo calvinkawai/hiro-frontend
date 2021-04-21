0ddd272085ec52d574880c3cd4602dd754eda71b
calvinkawai <ljw361@hotmail.com>
2021-04-21 12:35:03 -0400

    add register page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,17 @@
 import * as React from "react"
+import Register from './register'
+import { Link } from "gatsby"
 
 // markup
 const IndexPage = () => {
   return (
     <div>
       <nav class="navbar navbar-dark bg-dark">
-        <a class="navbar-brand" href="#home">HIRO</a>
-        <button type="button" class="btn btn-dark">Login</button>
+        <a class="navbar-brand">HIRO</a>
+        <Link to="/register" class="btn btn-secondary btn-lg" tabindex="-1" role="button" aria-disabled="true">
+          Register
+        </Link>
       </nav>
-      <samp>{process.env.GATSBY_APPLICATION_ID}</samp>
     </div>
   )
 }
