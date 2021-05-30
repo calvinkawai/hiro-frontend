59c07c834488ab4f2e91a349ef3d542c3e31ffff
Ariel Wang <ariel@Ariels-MacBook-Air.local>
2021-05-30 01:21:05 -0400

    change PrivateComp to function and add survey component

--- src/components/PrivateComp.js
+++ src/components/PrivateComp.js
@@ -1,40 +1,25 @@
-import * as React from "react"
-import { isLoggedIn, getCurrentUser } from "../services/auth"
+import React, { useState, useEffect } from "react"
+import PropTypes from "prop-types"
+import { isLoggedIn } from "../services/auth"
 import { navigate } from "gatsby"
-import { GetUserAvgTennisRating, GetUserTennisRatings, SetUserTennisRatings, SetUserLocation, FindNN } from "../services/user"
 
 const isBrowser = typeof window !== "undefined"
 
-class PrivateComp extends React.Component {
-  componentDidMount(prevProps) {
+const PrivateComp = ({ component: Component, location, ...rest }) => {
+
+  useEffect(() => {
     if (!isLoggedIn()) {
+      // If we’re not logged in, redirect to the home page.
       navigate(`/login`)
+      return null
     }
-    // How we would call user related services
-    // if ("geolocation" in navigator) {
-    //   navigator.geolocation.getCurrentPosition((position) => {
-    //     SetUserLocation(getCurrentUser(), position.coords.latitude, position.coords.longitude);
-    //   });
-    // } else {
-    //   console.log("location services Not Available");
-    // }
-    // SetUserTennisRatings(getCurrentUser(), 1, 2, 3, 4, 5, 6)
-    // FindNN(getCurrentUser()).then(results => {
-    //   console.log(results)
-    // })
-    // console.log(GetUserAvgTennisRating(getCurrentUser()))
-    // console.log(GetUserTennisRatings(getCurrentUser()))
-  }
-  render() {
-    let title;
-    if (isLoggedIn()) {
-      title = <h1>Shopping List {getCurrentUser().getUsername()}</h1>
-    }
-    return (
-      <div className="shopping-list">
-        {title}
-      </div>
-    )
-  }
+  })
+
+  return <Component {...rest} />
 }
+
+PrivateComp.propTypes = {
+  component: PropTypes.any.isRequired,
+}
+
 export default PrivateComp;
